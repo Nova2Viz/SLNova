e9268e16e3e9bcf36f52ce961e835478c511d52c
Tamilrockers19 <49454876+Tamilrockers19@users.noreply.github.com>
2019-10-18 07:51:24 +0530

    Create oursubnet.py

---
+++ oursubnet.py
@@ -0,0 +1,15 @@
+ip=input("enter the ip address")
+p=ip[:-1]
+s=int(input("enter the subnet mask"))
+net=s%24
+netno=pow(2,net)
+host=8-net
+hostno=pow(2,host)
+print("no of hosts",hostno-2)
+count=0
+for i in range(1,netno+1):
+        print("network ",i,"address-->",p,count,end="\n")
+        print("start-->",p,count+1,end="\n")
+        print("end--->",p,count+30,end="\n")
+        print(end="\n")
+        count=count+32
